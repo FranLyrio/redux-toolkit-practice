c2412c823e20caa77d5af25c0a65826bbdd1e9fe
Franciele Lyrio <franciele.moraes@viavarejo.com.br>
2022-09-05 08:56:21 -0300

    feat: cria template para estado counter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 
-import Button from '../components/Button'
+import { CounterTemplate } from '../templates/Counter'
 
 const Index: NextPage = () => {
-  return <Button onClick={() => console.log('Clicked')}>I'm a children</Button>
+  return <CounterTemplate />
 }
 
 export default Index
--- src/templates/Counter.tsx
+++ src/templates/Counter.tsx
@@ -1,15 +1,27 @@
 import { useAppDispatch } from "../app/features/hooks/useAppDispatch";
+import { useAppSelector } from "../app/features/hooks/useAppSelector";
 import { counterActions } from '../app/features/slices/counterSlice'
+import { counterSelector } from '../app/features/selector'
+
 import Button from "../components/Button";
 
 export const CounterTemplate = () => {
   const dispatch = useAppDispatch()
+  const counterState = useAppSelector(counterSelector)
+
+  const increment = () => {
+    dispatch(counterActions.increment())
+  }
 
   const decrement = () => {
-    dispatch()
+    dispatch(counterActions.decrement())
   }
 
   return (
-    <Button>Counter</Button>
+    <>
+      <Button onClick={increment}>Increment</Button>
+      {counterState}
+      <Button onClick={decrement}>Decrement</Button>
+    </>
   )
 }
