ee3c23c34886f505efe5bd2c5816b4b74d09a69f
Franciele Lyrio <franciele.moraes@viavarejo.com.br>
2022-09-05 08:47:32 -0300

    fix: conserta bug que impedia usar dispatch como função

--- src/templates/Counter.tsx
+++ src/templates/Counter.tsx
@@ -1,3 +1,15 @@
+import { useAppDispatch } from "../app/features/hooks/useAppDispatch";
+import { counterActions } from '../app/features/slices/counterSlice'
 import Button from "../components/Button";
 
-export const CounterTemplate = () => <Button>Counter</Button>
+export const CounterTemplate = () => {
+  const dispatch = useAppDispatch()
+
+  const decrement = () => {
+    dispatch()
+  }
+
+  return (
+    <Button>Counter</Button>
+  )
+}
